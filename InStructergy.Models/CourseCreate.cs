0584b4e662417c3e84a881fed014acace59ec1fa
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-10 18:02:33 -0400

    create models CourseCreate, CourseListItem - corresponding CourseController methods, Authorize 'Admin, Instructor' roles for CourseController.

---
+++ InStructergy.Models/CourseCreate.cs
@@ -0,0 +1,20 @@
+﻿using InStructergy.Data.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStructergy.Models
+{
+    public class CourseCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string InstructorId { get; set; }
+        //constraints on the Students property can be set here through annotation.
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ InStructergy.Models/CourseListItem.cs
@@ -0,0 +1,18 @@
+﻿using InStructergy.Data.EntityModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStructergy.Models
+{
+    public class CourseListItem
+    {
+        public string Name { get; set; }
+        
+        [Display(Name = "Students Enrolled")]
+        public List<Student> Students { get; set; }
+    }
+}
