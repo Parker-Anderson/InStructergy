6505b049c300d1e8093fb23550966370404948ee
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-11 14:41:31 -0400

    CreateCourse and GetCourses service methods

--- InStructergy.Services/CourseService.cs
+++ InStructergy.Services/CourseService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InStructergy.Data;
+using InStructergy.Data.EntityModels;
+using InStructergy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,48 @@ using System.Threading.Tasks;
 
 namespace InStructergy.Services
 {
-    class CourseService
+    public class CourseService
     {
+        private readonly Guid _userId;
+        public CourseService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateCourse(CourseCreate model)
+        {
+            var entity =
+                new Course()
+                {
+                    InstructorId = _userId,
+                    Name = model.Name,
+                    Students = model.Students
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Courses.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CourseListItem> GetCourses()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Courses
+                    .Where(e => e.InstructorId == _userId)
+                    .Select(
+                        e =>
+                        new CourseListItem
+                        {
+                            Id = e.Id,
+                            Name = e.Name,
+
+                        }
+                        );
+                return query.ToArray();
+            }
+        }
     }
 }
