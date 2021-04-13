b184241e40eaa0185e6588f2e48b1f354979ed1d
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-13 04:09:27 -0400

    CRUD for Student

--- InStructergy.MVC/Controllers/StudentController.cs
+++ InStructergy.MVC/Controllers/StudentController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using InStructergy.Models.StudentModels;
+using InStructergy.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +16,84 @@ namespace InStructergy.MVC.Controllers
         {
             return View();
         }
+        private StudentService CreateStudentService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new StudentService(userId);
+            return service;
+        }
+
+        //GET: Create 
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(StudentCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var service = CreateStudentService();
+            service.CreateStudent(model);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var svc = CreateStudentService();
+            var model = svc.GetStudentById(id);
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var service = CreateStudentService();
+            var detail = service.GetStudentById(id);
+            var model =
+                new StudentEdit
+                {
+                    Name = detail.Name,
+                    Files = detail.Files,
+                    GradePointAverage = detail.GradePointAverage,
+                    Courses = detail.Courses
+
+                };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, StudentEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.Id != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = CreateStudentService();
+            if (service.UpdateStudent(model))
+            {
+                TempData["SaveResult"] = "Student Saved.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Student could not be Created.");
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var svc = CreateStudentService();
+            var model = svc.GetStudentById(id);
+            return View(model);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult StudentDelete(int id)
+        {
+            var service = CreateStudentService();
+            service.DeleteStudent(id);
+            TempData["SaveResult"] = "Student Deleted";
+            return RedirectToAction("Index");
+        }
+
     }
 }
---
+++ InStructergy.Services/StudentService.cs
@@ -0,0 +1,112 @@
+﻿using InStructergy.Data;
+using InStructergy.Data.EntityModels;
+using InStructergy.Models.StudentModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InStructergy.Services
+{
+    public class StudentService
+    {
+        
+            private readonly Guid _userId;
+            public StudentService(Guid userId)
+            {
+                userId = _userId;
+            }
+            public bool CreateStudent(StudentCreate model)
+            {
+                var entity =
+                    new Student()
+                    {
+                        Id = model.Id,
+                        Name = model.Name,
+                        Files = model.Files,
+                        Courses = model.Courses,
+                        InstructorId = model.InstructorId,
+                        Instructors = model.Instructors,
+                        GradePointAverage = model.GradePointAverage
+                    };
+                using (var ctx = new ApplicationDbContext())
+                {
+                    ctx.Students.Add(entity);
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+            public IEnumerable<StudentListItem> GetStudents()
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var query =
+                        ctx
+                        .Students
+                        .Select(
+                            e =>
+                            new StudentListItem
+                            {
+                                Id = e.Id,
+                                Name = e.Name,
+                                Files = e.Files,
+                                Courses = e.Courses,
+                                InstructorId = e.InstructorId,
+                                Instructors = e.Instructors,
+                                GradePointAverage = e.GradePointAverage
+                            });
+                    return query.ToArray();
+                }
+            }
+            public StudentDetail GetStudentById(int id)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx
+                        .Students
+                        .Single(e => e.Id == id);
+                    return
+                        new StudentDetail
+                        {
+                            Id = entity.Id,
+                            Name = entity.Name,
+                            Files = entity.Files,
+                            Courses = entity.Courses,
+                            InstructorId = entity.InstructorId,
+                            Instructors = entity.Instructors,
+                            GradePointAverage = entity.GradePointAverage
+                        };
+
+
+                }
+            }
+            public bool UpdateStudent(StudentEdit model)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx
+                        .Students
+                        .Single(e => e.Id == model.Id);
+
+                    return ctx.SaveChanges() == 1;
+                }
+
+            }
+            public bool DeleteStudent(int id)
+            {
+                using (var ctx = new ApplicationDbContext())
+                {
+                    var entity =
+                        ctx.Students
+                        .Single(e => e.Id == id);
+                    ctx.Students.Remove(entity);
+                    return ctx.SaveChanges() == 1;
+                }
+            }
+        }
+
+    }
+}
+}
