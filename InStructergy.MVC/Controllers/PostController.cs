b744ae211756039e18e54614c3f1fc282c12a504
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-13 18:35:49 -0400

    Post Models, Controller, and Service

---
+++ InStructergy.MVC/Controllers/PostController.cs
@@ -0,0 +1,99 @@
+﻿using InStructergy.Models.PostModels;
+using InStructergy.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InStructergy.MVC.Controllers
+{
+    public class PostController : Controller
+    {
+        public ActionResult Index()
+        {
+            var service = CreatePostService();
+            var model = service.GetPosts();
+            return View(model);
+
+        }
+
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new PostService(userId);
+            return service;
+        }
+
+        //GET: Create 
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(PostCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var service = CreatePostService();
+            service.CreatePost(model);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            var svc = CreatePostService();
+            var model = svc.GetPostById(id);
+            return View(model);
+        }
+        public ActionResult Edit(int id)
+        {
+            var service = CreatePostService();
+            var detail = service.GetPostById(id);
+            var model =
+                new PostEdit
+                {
+
+                };
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, PostEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            if (model.Id != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+            var service = CreatePostService();
+            if (service.UpdatePost(model))
+            {
+                TempData["SaveResult"] = "Post Saved.";
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Post could not be Created.");
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var svc = CreatePostService();
+            var model = svc.GetPostById(id);
+            return View(model);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreatePostService();
+            service.DeletePost(id);
+            TempData["SaveResult"] = "Post Deleted";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
--- InStructergy.Models/PostModels/PostDetail.cs
+++ InStructergy.Models/PostModels/PostDetail.cs
@@ -1,23 +1,20 @@
-﻿using System;
+﻿using InStructergy.Data;
+using InStructergy.Data.EntityModels;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InStructergy.Data.EntityModels
+namespace InStructergy.Models.PostModels
 {
-    public class Post
+    public class PostDetail
     {
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Body { get; set; }
-        [ForeignKey(nameof(Instructor))]
         public string InstructorId { get; set; }
         public virtual ApplicationUser Instructor { get; set; }
-        [ForeignKey(nameof(Student))]
         public int StudentId { get; set; }
         public virtual Student Student { get; set; }
     }
