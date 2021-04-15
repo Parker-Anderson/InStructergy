e4aca163af18c95eb75c3ea9230126406bcd1903
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-15 19:38:25 -0400

    PostReply migration

--- SchoolBoard.Data/EntityModels/PostReply.cs
+++ SchoolBoard.Data/EntityModels/PostReply.cs
@@ -8,18 +8,14 @@ using System.Threading.Tasks;
 
 namespace SchoolBoard.Data.EntityModels
 {
-    public class Post
+    public class PostReply
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Body { get; set; }
-        public List<PostReply> Replies { get; set; }
         [ForeignKey(nameof(Instructor))]
         public string InstructorId { get; set; }
         public virtual ApplicationUser Instructor { get; set; }
-        [ForeignKey(nameof(Student))]
-        public int StudentId { get; set; }
-        public virtual Student Student { get; set; }
+        public string Label { get; set; }
+        public string Body { get; set; }
     }
 }
---
+++ SchoolBoard.Data/Migrations/202104152337187_PostReply.cs
@@ -0,0 +1,37 @@
+﻿namespace SchoolBoard.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PostReply : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PostReply",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        InstructorId = c.String(maxLength: 128),
+                        Label = c.String(),
+                        Body = c.String(),
+                        Post_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUser", t => t.InstructorId)
+                .ForeignKey("dbo.Post", t => t.Post_Id)
+                .Index(t => t.InstructorId)
+                .Index(t => t.Post_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.PostReply", "Post_Id", "dbo.Post");
+            DropForeignKey("dbo.PostReply", "InstructorId", "dbo.ApplicationUser");
+            DropIndex("dbo.PostReply", new[] { "Post_Id" });
+            DropIndex("dbo.PostReply", new[] { "InstructorId" });
+            DropTable("dbo.PostReply");
+        }
+    }
+}
