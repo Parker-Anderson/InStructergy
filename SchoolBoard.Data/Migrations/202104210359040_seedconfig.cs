59c721323b77a0f4d22ac145be559bc290d735c0
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-20 23:59:38 -0400

    .

---
+++ SchoolBoard.Data/Migrations/202104210359040_seedconfig.cs
@@ -0,0 +1,16 @@
+namespace SchoolBoard.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedconfig : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
