3696860a6de9b324a05b0d392f227ee3d158dee6
Parker Anderson <parker.anderson0810@outlook.com>
2021-04-22 15:46:41 -0400

    seed data update

--- SchoolBoard.Data/Migrations/Configuration.cs
+++ SchoolBoard.Data/Migrations/Configuration.cs
@@ -49,7 +49,17 @@ namespace SchoolBoard.Data.Migrations
                 new DomainModels.Student() { Id = 9, Name = "John", GradePointAverage = 2.0 },
                 new DomainModels.Student() { Id = 10, Name = "Amanda", GradePointAverage = 4.0 }
                 );
-            
+            context.Users.AddOrUpdate(c => c.Id,
+               new SchoolBoard.Data.ApplicationUser() { Id = "2371064d-3077-4bcb-a91d-ba89b1bc5562", Email = "test2@test.com", EmailConfirmed = false, PasswordHash = "AHSkgSapHhYyDjPxvoQMKL8D2QC5eQg6WvYu9fuvOhmL5Cn8DsgqqZwRSXeTbE2FRg==", SecurityStamp = "bc31cc00-992a-454c-ae19-03c89f6015d5", TwoFactorEnabled = false, LockoutEnabled = true, AccessFailedCount = 0, UserName = "Mrs. Robinson", Name = "Mrs. Robinson" },
+               new SchoolBoard.Data.ApplicationUser() { Id = "2729b303-5557-4e1a-8422-bc311b06ee17", Email = "teacher@teacher.com", EmailConfirmed = false, PasswordHash = "AGtN9rH5KsC7rGey47hwLzb4b4SY1YuDz+hvHB5LaG1UcierZqT23W7fgKUMLdR+bg==", SecurityStamp = "58a39ba7-6d83-42df-9158-687105b75dcd", TwoFactorEnabled = false, LockoutEnabled = true, AccessFailedCount = 0, UserName = "Mr. Jones", Name = "Mr. Jones" },
+               new SchoolBoard.Data.ApplicationUser() { Id = "cf45c79c-e064-485d-988d-bfb54e38174e", Email = "test@test.com", EmailConfirmed = false, PasswordHash = "ALIX3IjlG9TlnfHiWt8tbpv83YNboQEehEvv449dbt6PeBl7uSLNp7JRyondV460sg==", SecurityStamp = "0d965775-695c-49c9-8811-9240dd3b6137", TwoFactorEnabled = false, LockoutEnabled = true, AccessFailedCount = 0, UserName = "Mr. Joyce", Name = "Mr. Joyce" },
+               new SchoolBoard.Data.ApplicationUser() { Id = "d50bfa7c-0ca5-497c-b4d6-eb2d52c6357a", Email = "email@email.com", EmailConfirmed = false, PasswordHash = "ADwNL/a0J1cckDIDixHIfrtI15nvLQ0xp6vJtkW4iuRqp8/vchuAU2vH6uaf3ZcrmA==", SecurityStamp = "2fc12aec-5ab7-4ae5-8df5-a21f974f22f1", TwoFactorEnabled = false, LockoutEnabled = true, AccessFailedCount = 0, UserName = "Ms. Jones", Name = "Ms. Jones" }
+               );
+
+
+
+
+
         }
     }
 }
